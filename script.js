8eebb722372048153e08bb8c7eb08a5741e2952a
afrashaikh <162832706+afrashaikh@users.noreply.github.com>
2024-03-09 21:51:10 +0000

    Fixed spacing of the home page

--- script.js
+++ script.js
@@ -9,16 +9,22 @@ function foodTime(){
   return mealTimes;
 }
 function nutrional(){
-  var goals=[];
+  var goals=["protein","iron","fat","carbs","fiber","sugar","salt","calcium","vitamin A","vitamin B","vitamin C","vitamin D","vitamin E","vitamin K"]];
   //a drop down menu or maybe a check box that allows you to multi select as many goals as you have
    //protien, iron, fat, carbs, fiber, sugar, salt, calcium, vitamins, minerals
-  var protein = document.createElement("input");
-  protein.type = "checkbox";
-  protein.name = "goal";
-  protein.value="protein";
-  
-  var porteinLabel = document.createElement("label");
-  porteinLabel.innerHTML = "Protein";
+  var goalsC=document.createElemet("div");
+  // var protein = document.createElement("input");
+  // protien.type = "checkbox";
+  // protien.name = "goal";
+  // protien.value="protein";
+  goalsList.forEach(function(goal)){
+    var checkbox=document.createElement("input");
+    checkbpx.type = "checkbox";
+    checkbox.name = "goal";
+    checkbox.value=goal;
+  }
+  // var proteinLabel = document.createElement("label");
+  // porteinLabel.innerHTML = "Protein";
 
   proteinLabel.appendChild(document.createTextNode("Protein"));
     document.body.appendChild(protein);
