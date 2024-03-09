8edd1ae1c253652ea97c3e6b0dbdccecd6d0b0cf
nithisurender05 <162770560+nithisurender05@users.noreply.github.com>
2024-03-09 22:50:17 +0000

    Added checkboxes for dietary restrictions and nutritional goals

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function foodTime(){
 }
 function nutrional(){
   var checkedGoals=[];
-  var checkedboxes= document.querySelectorAll('input[name="goal"]:checked')
+  var checkedboxes= document.querySelectorAll('input[name="goal"]:checked');
   //a drop down menu or maybe a check box that allows you to multi select as many goals as you have
    //protien, iron, fat, carbs, fiber, sugar, salt, calcium, vitamins, minerals
   checkedboxes.forEach(function(checkbox){
@@ -18,12 +18,28 @@ function nutrional(){
   });
   localStorage.setItem('selectedNutritionalGoals' , JSON.stringify(checkedGoals));
 }
+function dietry(){
+  var checkedDiet=[];
+  var checked=document.querySelectorAll('input[name="diet"]:checked');
+  checkedboxes.forEach(function(checkbox){
+    checkedGoals.push(checkbox.value);
+  });
+  localStorage.setItem('selectedDietaryRestrictions' , JSON.stringify(checkedDiet));
+}
+
 
 document.addEventListner("DOMContentLoaded",function(){
   var question1Form=document.getElementById("question1Form");
+  var question4Form=document.getElementById("question4Form")
   if(question1Form){
     question1Form.addEventListener("change",function(){
      nutrional();
     });
   }
+  if(question3Form){
+    question3Form.addEventListener("change",function(){
+     dietry();
+    });
+  }
+  
 }
