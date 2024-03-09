b54343a97e6506d984bbe74a0e9e1737f465d80b
abbysavio <162770326+abbysavio@users.noreply.github.com>
2024-03-09 22:31:41 +0000

    finshed function that stores input of q1git add .

--- script.js
+++ script.js
@@ -9,43 +9,16 @@ function foodTime(){
   return mealTimes;
 }
 function nutrional(){
-  var goals=["protein","iron","fat","carbs","fiber","sugar","salt","calcium","vitamin A","vitamin B","vitamin C","vitamin D","vitamin E","vitamin K"]];
+  var checkedGoals=[];
+  var checkedboxes= document.querySelectorAll('input[name="goal"]:checked')
   //a drop down menu or maybe a check box that allows you to multi select as many goals as you have
    //protien, iron, fat, carbs, fiber, sugar, salt, calcium, vitamins, minerals
-  var goalsC=document.createElemet("div");
-  // var protein = document.createElement("input");
-  // protien.type = "checkbox";
-  // protien.name = "goal";
-  // protien.value="protein";
-  goalsList.forEach(function(goal)){
-    var checkbox=document.createElement("input");
-    checkbpx.type = "checkbox";
-    checkbox.name = "goal";
-    checkbox.value=goal;
-  }
-  // var proteinLabel = document.createElement("label");
-  // porteinLabel.innerHTML = "Protein";
-
-  proteinLabel.appendChild(document.createTextNode("Protein"));
-    document.body.appendChild(protein);
-    document.body.appendChild(proteinLabel);
-    document.body.appendChild(document.createElement("br"))
-
-  document.querySelectorAll('input[name="goal]"').forEach(checkbox)=>{
-    checkbox.addEventListner("change",function(){
-      if(this.checked){
-        goals.push(this.value);
-      } 
-      else{
-        var index=goals.indexOf(this.value);
-        if(index !===-1){
-          goals.splice(index,1);
-        } 
-      }
-    }
-    
-  }
+  checkedboxes.forEach(function(checkbox){
+    checkedGoals.push(checkbox.value);
+  });
+  localStorage.setItem('selectedNutritionalGoals' , JSON.stringify(checkedGoals));
 }
+
 document.addEventListner("DOMContentLoaded",function(){
   var question1Form=document.getElementById("question1Form");
   if(question1Form){
