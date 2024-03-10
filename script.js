4d61a019a17817a32db9e0ef3fdc2e42aa8c74e6
galaxywave49 <26668001-galaxywave49@users.noreply.replit.com>
2024-03-10 00:08:18 +0000

    finshed functions that store value of the question

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 
 function foodTime(){
-  var mealTimes=[];
-  var quanity = document.getElementById("quantity").value;
-  for(let i=0;i<quanity;i++){
-    var time = prompt("Enter meal times "+(i+1)+":");
-    mealTimes.push(time);
+  var breakfast= document.getElementById("breakfast").value;
+  var lunch= document.getElementById("lunch").value;
+  var dinner= document.getElementById("dinner").value;
+  if(breakfast&&lunch&&dinner){
+    localStorage.setItem('breakfast',breakfast);
+    localStorage.setItem('lunch',lunch);
+    localStorage.setItem('dinner',dinner);
   }
-  return mealTimes;
+  else{
+    alert("Plese enter a time for each meal");
+  }
 }
+//a drop down menu or maybe a check box that allows you to multi select as many goals as you have
+ //protien, iron, fat, carbs, fiber, sugar, salt, calcium, vitamins, minerals
 function nutrional(){
   var checkedGoals=[];
   var checkedboxes= document.querySelectorAll('input[name="goal"]:checked');
-  //a drop down menu or maybe a check box that allows you to multi select as many goals as you have
-   //protien, iron, fat, carbs, fiber, sugar, salt, calcium, vitamins, minerals
   checkedboxes.forEach(function(checkbox){
     checkedGoals.push(checkbox.value);
   });
@@ -20,17 +24,17 @@ function nutrional(){
 }
 function dietry(){
   var checkedDiet=[];
-  var checked=document.querySelectorAll('input[name="diet"]:checked');
+  var checkedboxes=document.querySelectorAll('input[name="diet"]:checked');
   checkedboxes.forEach(function(checkbox){
-    checkedGoals.push(checkbox.value);
+    checkedDiet.push(checkbox.value);
   });
   localStorage.setItem('selectedDietaryRestrictions' , JSON.stringify(checkedDiet));
 }
 
 
-document.addEventListner("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded",function(){
   var question1Form=document.getElementById("question1Form");
-  var question4Form=document.getElementById("question4Form")
+  var question3Form=document.getElementById("question3Form");
   if(question1Form){
     question1Form.addEventListener("change",function(){
      nutrional();
